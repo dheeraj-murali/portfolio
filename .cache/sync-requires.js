40c2ce17d15df6c52879ffe6468379f4d95e51ba
Dheeraj <410dheeraj@gmail.com>
2020-10-12 18:18:29 +0530

    installed chakra

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/Dheeraj/Documents/GitHub/portfolio/.cache/dev-404-page.js"))),
+  "component---src-pages-index-tsx": hot(preferDefault(require("/Users/Dheeraj/Documents/GitHub/portfolio/src/pages/index.tsx")))
+}
+
