9fd7be0c15ebd6b44ab7e9ded683fc4cec04cdd4
Dheeraj <410dheeraj@gmail.com>
2020-10-13 10:27:44 +0530

    reset

---
+++ site/gatsby-config.js
@@ -0,0 +1,20 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+    `gatsby-plugin-chakra-ui`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+}
