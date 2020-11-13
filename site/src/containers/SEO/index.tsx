bdd7e8e0c0ac10edfb75c581069150d0e7e1b290
Dheeraj <410dheeraj@gmail.com>
2020-11-13 16:28:09 +0530

    feat: SEO
    - updated seo content

--- site/src/containers/SEO/index.tsx
+++ site/src/containers/SEO/index.tsx
@@ -10,6 +10,9 @@ export const SEO = (props: SEOProps) => {
     <Helmet title={title} titleTemplate={title}>
       <meta name="description" content={description} />
       <meta name="image" content={`${siteUrl}${image}`} />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
       {`${siteUrl}${pathname}` && (
         <meta property="og:url" content={`${siteUrl}${pathname}`} />
       )}
@@ -18,11 +21,16 @@ export const SEO = (props: SEOProps) => {
       {`${siteUrl}${image}` && (
         <meta property="og:image" content={`${siteUrl}${image}`} />
       )}
+
+      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
+      {`${siteUrl}${pathname}` && (
+        <meta property="twitter:url" content={`${siteUrl}${pathname}`} />
+      )}
+      {title && <meta name="twitter:title" content={title} />}
       {twitterUsername && (
         <meta name="twitter:creator" content={twitterUsername} />
       )}
-      {title && <meta name="twitter:title" content={title} />}
       {description && <meta name="twitter:description" content={description} />}
       {`${siteUrl}${image}` && (
         <meta name="twitter:image" content={`${siteUrl}${image}`} />
