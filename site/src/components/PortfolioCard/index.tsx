1006d07219444c02d474565a10789b70f91925ef
Dheeraj <410dheeraj@gmail.com>
2020-11-19 20:28:09 +0530

    theme update

--- site/src/components/PortfolioCard/index.tsx
+++ site/src/components/PortfolioCard/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, useDisclosure } from "@chakra-ui/react"
+import { Button, DarkMode, Flex, Text, useDisclosure } from "@chakra-ui/react"
 import Img from "gatsby-image"
 import React from "react"
 import { PortfolioCardProps } from "../../types"
@@ -7,35 +7,42 @@ import { PortfolioModal } from "../PortfolioModal"
 
 export const PortfolioCard = (props: PortfolioCardProps) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const { image, title, edges } = props
+  const { image, title, color, edges } = props
 
   return (
     <>
-      <Box
-        as={Button}
-        rounded="sm"
-        overflow="hidden"
-        objectFit="cover"
-        _hover={{ boxShadow: "5px 5px 15px #000000" }}
-        _focus={{ boxShadow: "5px 5px 15px #000000" }}
-        style={{ width: "100%", height: "100%" }}
-        onClick={onOpen}
-        p="0"
-      >
-        <Img
-          fluid={getFluid(edges, image)}
-          alt={`${title} screenshot`}
+      <DarkMode>
+        <Flex
+          flexDir="column"
+          as={Button}
+          overflow="hidden"
+          objectFit="cover"
+          _hover={{ boxShadow: `0px 0px 20px ${color}` }}
+          _focus={{ boxShadow: `5px 5px 15px ${color}` }}
           style={{ width: "100%", height: "100%" }}
-        />
-      </Box>
+          onClick={onOpen}
+          color={`${color}.300`}
+          borderColor={`${color}.300`}
+          variant="solid"
+          p="0"
+        >
+          <Img
+            fluid={getFluid(edges, image)}
+            alt={`${title} screenshot`}
+            style={{ width: "100%", height: "100%" }}
+          />
 
-      <PortfolioModal
-        isOpen={isOpen}
-        onOpen={onOpen}
-        onClose={onClose}
-        {...props}
-        fluid={getFluid(edges, image)}
-      />
+          <Text py="5">{title}</Text>
+        </Flex>
+
+        <PortfolioModal
+          isOpen={isOpen}
+          onOpen={onOpen}
+          onClose={onClose}
+          {...props}
+          fluid={getFluid(edges, image)}
+        />
+      </DarkMode>
     </>
   )
 }
