81dbbdfe9fffafc31a76e59c2ada81e3eb3f7670
Dheeraj Murali <410dheeraj@gmail.com>
2025-04-17 21:06:14 +0530

    added title to portfolio section

--- app/components/portfolio/portfolio.tsx
+++ app/components/portfolio/portfolio.tsx
@@ -5,13 +5,14 @@ const portfolio = data.portfolio;
 
 export const Portfolio = () => {
   return (
-    <section
-      id="portfolio"
-      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4"
-    >
-      {portfolio.map((item) => (
-        <PortfolioCard {...item} key={item.title} />
-      ))}
+    <section id="portfolio" className="px-4 flex flex-col gap-4">
+      <h2 className="text-xl font-bold">These are some of my work</h2>
+
+      <article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {portfolio.map((item) => (
+          <PortfolioCard {...item} key={item.title} />
+        ))}
+      </article>
     </section>
   );
 };
