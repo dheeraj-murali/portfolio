9accc11c074f8189f52a3c1a51739366ec9d0e51
Dheeraj Murali <410dheeraj@gmail.com>
2025-04-20 11:05:01 +0530

    about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { About } from "@/components/sections/about";
 import { Header } from "@/components/sections/header";
 import { Hero } from "@/components/sections/hero";
 import { Portfolio } from "@/components/sections/portfolio";
 
 function App() {
   return (
-    <main className="container mx-auto my-10 gap-20">
+    <main className="container mx-auto my-20 flex flex-col gap-40">
       <Header />
       <Hero />
       <Portfolio />
+      <About />
     </main>
   );
 }
