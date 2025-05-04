834d9c39d6e3a1ce61ca6907b90ced1874c39a36
Dheeraj Murali <410dheeraj@gmail.com>
2025-05-04 22:18:49 +0530

    fix preloading

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   async prerender() {
-    return ["/", "/blog"];
+    return ["/", "/blog/:slug"];
   },
 } satisfies Config;
