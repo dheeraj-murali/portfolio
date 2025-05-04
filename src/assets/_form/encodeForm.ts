8eb9183fa64f4155c6c329849af84bbc54d66583
Dheeraj Murali <410dheeraj@gmail.com>
2025-05-04 13:33:20 +0530

    clean slate

--- src/assets/_form/encodeForm.ts
+++
@@ -1,8 +0,0 @@
-// export const encode = (data: EncodeForm) => {
-//   return Object.keys(data)
-//     .map((key) => {
-//       // @ts-ignore
-//       return encodeURIComponent(key) + "=" + encodeURIComponent(data[key]);
-//     })
-//     .join("&");
-// };
