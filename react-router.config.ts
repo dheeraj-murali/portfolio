ce252fc9c4623a7ce1c70386ad1a2da1240210ff
Dheeraj Murali <410dheeraj@gmail.com>
2025-05-04 13:49:15 +0530

    enabling pre rendering

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  async prerender() {
+    return ["/", "/blog"];
+  },
 } satisfies Config;
