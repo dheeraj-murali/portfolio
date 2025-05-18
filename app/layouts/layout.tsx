d442308e996a9b7caf52916925898c72a643c01f
Dheeraj Murali <410dheeraj@gmail.com>
2025-05-18 22:09:02 +0530

    moving layouts

--- app/layouts/layout.tsx
+++ app/layouts/layout.tsx
@@ -1,12 +1,15 @@
+import { Outlet } from "react-router";
 import { Footer } from "~/components/sections/footer";
 import { Header } from "~/components/sections/header";
 import { Toaster } from "~/components/ui/sonner";
 
-export const AppLayout = ({ children }: { children: React.ReactNode }) => {
+export const AppLayout = () => {
   return (
-    <main className="container mx-auto my-20 flex flex-col gap-40">
+    <main className="container mx-auto my-20 px-4 flex flex-col gap-40">
       <Header />
-      {children}
+
+      <Outlet />
+
       <Footer />
       <Toaster />
     </main>
