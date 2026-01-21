748e6e99515126ffa105f7719a0018c5ec91f6bc
Dheeraj <410dheeraj@gmail.com>
2026-01-21 18:30:25 +0530

    Portfolio update (#52)
    * adding new UI components
    
    * style: update color variables for improved theme consistency
    
    * new set of icons
    
    * feat: add tooltips for tech stack icons
    
    * feat: add new portfolio images
    
    * feat: enhance portfolio card with dialog and update data source
    
    * feat: remove outdated portfolio entries and add new projects
    
    * feat: add Redux and SWR icons as SVG components
    
    * feat: add Redux and SWR icons with updated transition styles
    
    * feat: add tags display to portfolio card component
    
    * feat: update portfolio entries with enhanced descriptions and tags
    
    * refactor: comment out unused tags display in portfolio card component

--- app/components/sections/portfolio/portfolio-card.tsx
+++ app/components/sections/portfolio/portfolio-card.tsx
@@ -2,7 +2,7 @@ import { ExternalLinkIcon } from "lucide-react";
 
 import { cn } from "~/lib/utils";
 import { TechStack } from "~/components/techStack";
-import { buttonVariants } from "~/components/ui/button";
+import { Button, buttonVariants } from "~/components/ui/button";
 import {
   Card,
   CardHeader,
@@ -10,47 +10,123 @@ import {
   CardContent,
   CardDescription,
   CardFooter,
+  CardAction,
 } from "~/components/ui/card";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { ScrollArea } from "~/components/ui/scrollArea";
+import { Badge } from "~/components/ui/badge";
 
 type PortfolioCardProps = {
   title: string;
+  client?: string;
   image: string;
-  description: string;
+  description: string[];
   tech: string[];
-  link: string;
+  tags: string[];
+  year?: string;
+  link?: string;
 };
 
 export const PortfolioCard = ({
   title,
+  client,
   image,
   description,
   tech,
+  year,
+  tags,
   link,
 }: PortfolioCardProps) => (
   <Card key={title} className="w-full h-full flex flex-col justify-between">
     <CardHeader>
-      <CardTitle className="flex items-end gap-2">
-        <img
-          src={image}
-          alt={title}
-          className="size-10 rounded object-fill border"
-        />
-        <h3 className="text-lg font-bold">{title}</h3>
+      <CardTitle className="flex items-center gap-2">
+        <img src={image} alt={title} className="size-12 rounded object-fill" />
+        <h3 className="text-lg font-bold capitalize">{title}</h3>
       </CardTitle>
+      {/* {tags.length > 0 && (
+        <div className="flex flex-row flex-wrap gap-2 mt-2">
+          {tags.map((tag) => (
+            <Badge
+              variant="outline"
+              className="uppercase text-[0.64rem] bg-muted"
+              key={tag}
+            >
+              {tag}
+            </Badge>
+          ))}
+        </div>
+      )} */}
     </CardHeader>
     <CardContent>
-      <CardDescription className="line-clamp-3">{description}</CardDescription>
+      <CardDescription className="line-clamp-3">
+        {description[0]}
+      </CardDescription>
       <TechStack stack={tech} />
     </CardContent>
     <CardFooter className="flex justify-end">
-      <a
-        href={link}
-        target="_blank"
-        rel="noreferrer"
-        className={cn(buttonVariants({ variant: "link" }))}
-      >
-        View Project <ExternalLinkIcon className="size-4 mb-0.5" />
-      </a>
+      <Dialog>
+        <DialogTrigger>
+          <Button variant="outline">Read More</Button>
+        </DialogTrigger>
+        <DialogContent className="p-10 flex flex-col gap-10 max-w-3xl!">
+          <div>
+            <div className="flex flex-row items-center gap-2">
+              <img
+                src={image}
+                alt={title}
+                className="size-12 rounded object-fill"
+              />
+              <div className="flex flex-col gap-0">
+                <DialogTitle className="text-lg font-bold capitalize">
+                  {title}
+                </DialogTitle>
+                <DialogDescription>{client}</DialogDescription>
+              </div>
+            </div>
+            {/* {tags.length > 0 && (
+              <div className="flex flex-row flex-wrap gap-2 mt-2">
+                {tags.map((tag) => (
+                  <Badge
+                    variant="outline"
+                    className="uppercase text-[0.64rem] bg-muted"
+                    key={tag}
+                  >
+                    {tag}
+                  </Badge>
+                ))}
+              </div>
+            )} */}
+          </div>
+          <ScrollArea className="h-full max-h-[calc(100vh-24rem)] flex flex-col gap-4">
+            {description.map((desc, index) => (
+              <p key={index} className="mt-4">
+                {desc}
+              </p>
+            ))}
+          </ScrollArea>
+          <TechStack stack={tech} />
+          <DialogFooter>
+            {link && (
+              <a
+                href={link}
+                target="_blank"
+                rel="noreferrer"
+                className={cn(buttonVariants({ variant: "link" }))}
+              >
+                Visit client <ExternalLinkIcon className="size-4 mb-0.5" />
+              </a>
+            )}
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </CardFooter>
   </Card>
 );
--- app/components/techStack.tsx
+++ app/components/techStack.tsx
@@ -12,6 +12,13 @@ import { ReactRouterIcon } from "~/components/icons/reactrouter";
 import { ShadcnIcon } from "~/components/icons/shadcn";
 import { TailwindIcon } from "~/components/icons/tailwind";
 import { TypescriptIcon } from "~/components/icons/typescript";
+import { PlotlyIcon } from "./icons/plotly";
+import { ElectronIcon } from "./icons/electron";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import { ChartIcon } from "./icons/chartjs";
+import { ReduxIcon } from "./icons/redux";
+import { SwrIcon } from "./icons/swr";
+import { cn } from "~/lib/utils";
 
 type TechStackProps = {
   stack?: string[];
@@ -22,48 +29,234 @@ const isIncluded = (stack: string[] | undefined, item: string) =>
 
 export const TechStack = ({ stack }: TechStackProps) => {
   return (
-    <div className="flex flex-row items-baseline gap-4 mt-4">
+    <div className="flex flex-row flex-wrap items-baseline gap-4 mt-4">
       {isIncluded(stack, "js") && (
-        <JavascriptIcon className="size-4 fill-[#F7DF1E] delay-0 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <JavascriptIcon
+              className={cn(
+                "size-4 fill-[#F7DF1E] delay-0 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>JavaScript</TooltipContent>
+        </Tooltip>
       )}
       {isIncluded(stack, "ts") && (
-        <TypescriptIcon className="size-4 fill-[#3178C6] delay-100 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <TypescriptIcon
+              className={cn(
+                "size-4 fill-[#3178C6] delay-100 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>TypeScript</TooltipContent>
+        </Tooltip>
       )}
       {isIncluded(stack, "css") && (
-        <CssIcon className="size-4 fill-[#663399] delay-200 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <CssIcon
+              className={cn(
+                "size-4 fill-[#663399] delay-200 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>CSS</TooltipContent>
+        </Tooltip>
       )}
       {isIncluded(stack, "btsrp") && (
-        <BootstrapIcon className="size-4 fill-[#7952B3] delay-300 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <BootstrapIcon
+              className={cn(
+                "size-4 fill-[#7952B3] delay-300 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>Bootstrap</TooltipContent>
+        </Tooltip>
       )}
       {isIncluded(stack, "tw") && (
-        <TailwindIcon className="size-4 fill-[#06B6D4] delay-400 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <TailwindIcon
+              className={cn(
+                "size-4 fill-[#06B6D4] delay-400 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>Tailwind CSS</TooltipContent>
+        </Tooltip>
       )}
       {isIncluded(stack, "rt") && (
-        <ReactIcon className="size-4 fill-[#61DAFB] delay-500 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <ReactIcon
+              className={cn(
+                "size-4 fill-[#61DAFB] delay-500 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>React</TooltipContent>
+        </Tooltip>
       )}
       {isIncluded(stack, "scn") && (
-        <ShadcnIcon className="size-4 fill-[#000000] dark:fill-[#ffffff] delay-600 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <ShadcnIcon
+              className={cn(
+                "size-4 fill-[#00000{(0] dark:fill-[#ffffff] delay-600 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>Shadcn</TooltipContent>
+        </Tooltip>
       )}
       {isIncluded(stack, "rtr") && (
-        <ReactRouterIcon className="size-4 fill-[#CA4245] delay-700 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <ReactRouterIcon
+              className={cn(
+                "size-4 fill-[#CA4245] delay-700 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>React Router</TooltipContent>
+        </Tooltip>
       )}
       {isIncluded(stack, "rtq") && (
-        <ReactQueryIcon className="size-4 fill-[#FF4154] delay-800 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <ReactQueryIcon
+              className={cn(
+                "size-4 fill-[#FF4154] delay-800 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>React Query</TooltipContent>
+        </Tooltip>
+      )}
+      {isIncluded(stack, "swr") && (
+        <Tooltip>
+          <TooltipTrigger>
+            <SwrIcon
+              className={cn(
+                "size-4 fill-[#00000{(0] dark:fill-[#ffffff] delay-800 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>SWR</TooltipContent>
+        </Tooltip>
+      )}
+      {isIncluded(stack, "rdx") && (
+        <Tooltip>
+          <TooltipTrigger>
+            <ReduxIcon
+              className={cn(
+                "size-4 fill-[#764ABC] delay-800 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>Redux</TooltipContent>
+        </Tooltip>
       )}
       {isIncluded(stack, "gql") && (
-        <GraphqlIcon className="size-4 fill-[#E10098] delay-900 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <GraphqlIcon
+              className={cn(
+                "size-4 fill-[#E10098] delay-900 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>GraphQL</TooltipContent>
+        </Tooltip>
       )}
       {isIncluded(stack, "njs") && (
-        <NextjsIcon className="size-4 fill-[#000000] dark:fill-[#ffffff] delay-1000 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <NextjsIcon
+              className={cn(
+                "size-4 fill-[#00000{(0] dark:fill-[#ffffff] delay-1000 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>Next.js</TooltipContent>
+        </Tooltip>
       )}
       {isIncluded(stack, "gtpy") && (
-        <GatsbyIcon className="size-4 fill-[#663399] delay-1100 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <GatsbyIcon
+              className={cn(
+                "size-4 fill-[#663399] delay-1100 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>Gatsby</TooltipContent>
+        </Tooltip>
       )}
       {isIncluded(stack, "ntfy") && (
-        <NetlifyIcon className="size-4 fill-[#00C7B7] delay-1200 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <NetlifyIcon
+              className={cn(
+                "size-4 fill-[#00C7B7] delay-1200 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>Netlify</TooltipContent>
+        </Tooltip>
       )}
       {isIncluded(stack, "aws") && (
-        <AwsIcon className="size-4 fill-[#232F3E] dark:fill-[#ffffff] delay-1300 animate-landing transition-transform" />
+        <Tooltip>
+          <TooltipTrigger>
+            <AwsIcon
+              className={cn(
+                "size-4 fill-[#232F3{(E] dark:fill-[#ffffff] delay-1300 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>AWS</TooltipContent>
+        </Tooltip>
+      )}
+      {isIncluded(stack, "plt") && (
+        <Tooltip>
+          <TooltipTrigger>
+            <PlotlyIcon
+              className={cn(
+                "size-4 fill-[#7A76FF] delay-1400 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>Plotly</TooltipContent>
+        </Tooltip>
+      )}
+      {isIncluded(stack, "elt") && (
+        <Tooltip>
+          <TooltipTrigger>
+            <ElectronIcon
+              className={cn(
+                "size-4 fill-[#47848F] delay-1500 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>Electron</TooltipContent>
+        </Tooltip>
+      )}
+      {isIncluded(stack, "chrt") && (
+        <Tooltip>
+          <TooltipTrigger>
+            <ChartIcon
+              className={cn(
+                "size-4 fill-[#47848F] delay-1500 animate-landing transition-all"
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>Chart.js</TooltipContent>
+        </Tooltip>
       )}
     </div>
   );
