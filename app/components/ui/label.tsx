658a97d104e8170108fb16055cb844b4c333ef4a
Dheeraj Murali <410dheeraj@gmail.com>
2026-02-22 10:58:35 +0530

    feat: refactor UI components to use radix-ui imports and improve styling

--- app/components/ui/label.tsx
+++ app/components/ui/label.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as LabelPrimitive from "@radix-ui/react-label";
+import { Label as LabelPrimitive } from "radix-ui";
 
 import { cn } from "~/lib/utils";
 
@@ -12,7 +12,7 @@ function Label({
       data-slot="label"
       className={cn(
         "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
-        className
+        className,
       )}
       {...props}
     />
